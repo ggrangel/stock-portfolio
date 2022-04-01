9e4320d9066ebf3c254c6abd13d36bfe918e8c27
Gustavo Rangel <gustavorangel91@gmail.com>
2022-04-01 17:48:44 -0300

    Render table.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,30 @@
 class Portfolio extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      portfolio: [
+        {
+          name: "Feetbook",
+          shares_owned: 20,
+          cost_per_share: 50,
+          market_price: 130,
+        },
+        {
+          name: "Yamazon",
+          shares_owned: 5,
+          cost_per_share: 200,
+          market_price: 500,
+        },
+        {
+          name: "Snoozechat",
+          shares_owned: 100,
+          cost_per_share: 20,
+          market_price: 3,
+        },
+      ],
+    };
+
+    // Note: api JSON data often come in underscore_styled like above
   }
   render() {
     return (
@@ -20,7 +44,43 @@ class Portfolio extends React.Component {
                   <th scope="col"></th>
                 </tr>
               </thead>
-              <tbody></tbody>
+              <tbody>
+                {portfolio.map((stock, index) => {
+                  const { name, shares_owned, cost_per_share, market_price } =
+                    stock;
+                  return (
+                    <tr key={index}>
+                      <td>{name}</td>
+                      <td>
+                        <input
+                          type="number"
+                          name="shares_owned"
+                          value={shares_owned}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="number"
+                          name="cost_per_share"
+                          value={cost_per_share}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="number"
+                          name="market_price"
+                          value={market_price}
+                        />
+                      </td>
+                      <td></td>
+                      <td></td>
+                      <td>
+                        <button className="btn btn-light btn-sm">remove</button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
             </table>
           </div>
         </div>
